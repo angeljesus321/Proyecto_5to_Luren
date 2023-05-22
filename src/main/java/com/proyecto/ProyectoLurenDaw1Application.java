ffc46e3d9b9c3a1598abba4df61c2604c039ac58
Angel Jesus Quispe Cachuan <I202115464@cibertec.edu.pe>
2023-05-21 20:35:45 -0500

    Creacion de proyecto DAW1 Luren

---
+++ src/main/java/com/proyecto/ProyectoLurenDaw1Application.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLurenDaw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLurenDaw1Application.class, args);
+	}
+
+}
